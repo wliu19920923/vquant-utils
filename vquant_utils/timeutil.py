cac90fafee723426ac08dad0f415cc83dcd4cfc6
wliu19920923@163.com <wliu19920923@163.com>
2022-12-06 04:14:18 +0800

    1.12.6

--- vquant_utils/timeutil.py
+++ vquant_utils/timeutil.py
@@ -4,5 +4,9 @@ from datetime import datetime
 TimeFormatStr = '%Y-%m-%d %H:%M:%S'
 
 
+def datetime_str_to_microsecond(datetime_str, format=TimeFormatStr):
+    return int(datetime.strptime(datetime_str, format).timestamp() * 1000)
+
+
 def microsecond_to_datetime_str(microseconds):
     return datetime.fromtimestamp(math.floor(microseconds / 1000)).strftime(TimeFormatStr)

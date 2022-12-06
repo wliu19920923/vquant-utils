47a9507df8e8dd07dc51a49e07e2f89f712eee73
wliu19920923@163.com <wliu19920923@163.com>
2022-12-07 06:44:06 +0800

    v1.12.7

--- vquant_utils/__init__.py
+++ vquant_utils/__init__.py
@@ -1,11 +1,37 @@
 from decimal import Decimal
 
 
-def retry(count):
-    """
-    重试装饰器
-    """
+class Order:
+    Buy, Sell = ('buy', 'sell')
+    Sides = (Buy, Sell)
 
+    Limit, Market = ('limit', 'market')
+    Types = (Limit, Market)
+
+    GTC, IOC, FOK = ('GTC', 'IOC', 'FOK')
+    Lines = (GTC, IOC, FOK)
+
+    Created, Submitted, PartialFilled, Filled, Cancelling, PartialCanceled, Canceled, Rejected, Expired = (
+        'created', 'submitted', 'partial_filled', 'filled', 'cancelling', 'partial_canceled',
+        'canceled', 'rejected', 'expired'
+    )
+    Status = (Created, Submitted, PartialFilled, Filled, Cancelling, PartialCanceled, Canceled, Rejected, Expired)
+
+
+class Position:
+    BOTH, LONG, SHORT = ('BOTH', 'LONG', 'SHORT')
+    Directions = (BOTH, LONG, SHORT)
+
+    ISOLATED, CROSSED = ('ISOLATED', 'CROSSED')
+    MarginMolds = (ISOLATED, CROSSED)
+
+
+class CandleLine:
+    M1, M5, M15, M30, H, D, W, M = ('1min', '5min', '15min', '30min', 'H', 'D', 'W', 'M')
+    Intervals = (M1, M5, M15, M30, H, D, W, M)
+
+
+def retry(count):
     def wrapper(func):
         def decorate(*args, **kwargs):
             for i in range(count):

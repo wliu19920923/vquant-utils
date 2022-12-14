1351282b41469d84f4feaf5c6747008fee516017
wliu19920923@163.com <wliu19920923@163.com>
2022-12-14 11:47:34 +0800

    v1.12.14

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ REQUIRES = ['aiohttp', 'motor', 'requests', 'tornado', 'pycryptodome', 'pyjwt']
 
 setup(
     name='vquant_utils',
-    version='1.12.13',
+    version='1.12.14',
     description='vquant_utils',
     platforms='Independant',
     zip_safe=False,
